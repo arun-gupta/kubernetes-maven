55d90c111c887a582fbd89822fa29190785b6b37
Arun Gupta <arun.gupta@gmail.com>
2017-01-13 12:41:09 -0500

    cleaning up files for a simple structure

--- src/main/java/org/javaee7/sample/MyApplication.java
+++
@@ -1,9 +0,0 @@
-package org.javaee7.sample;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/resources")
-public class MyApplication extends Application {
-
-}
